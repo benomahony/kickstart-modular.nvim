ca8dfe1433162de0f31fc0fcfeaa4a63628be1c3
Ben O'Mahony <ben@jobsasi.com>
2024-10-11 15:18:00 +0100

    Adding harpoon and making neo-tree rhs on start

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -9,12 +9,21 @@ return {
         'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
         'MunifTanjim/nui.nvim',
     },
+    lazy = false,
     cmd = 'Neotree',
     keys = {
-        { '\\', ':Neotree right reveal<CR>', desc = 'NeoTree reveal', silent = true },
+        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
     },
     opts = {
+        window = {
+            position = 'right',
+        },
         filesystem = {
+            filtered_items = {
+                visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                hide_dotfiles = false,
+                hide_gitignored = false,
+            },
             window = {
                 mappings = {
                     ['\\'] = 'close_window',
