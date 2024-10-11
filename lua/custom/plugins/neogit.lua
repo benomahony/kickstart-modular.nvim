c0651baba2e8036bd6fa81c9a8af3d2d3e82aac9
Ben O'Mahony <ben@jobsasi.com>
2024-10-11 17:48:42 +0100

    Reverting neogit config back

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -8,57 +8,21 @@ return {
         'ibhagwan/fzf-lua', -- optional
         'echasnovski/mini.pick', -- optional
     },
-    config = function()
-        local neogit = require 'neogit'
-        neogit.setup {
-            kind = 'vsplit',
-            integrations = {
-                diffview = true,
-            },
-            -- Automatically open the diff view when opening Neogit
-            auto_show_console = true,
-            console_timeout = 2000,
-            -- Custom status buffer configuration
-            status = {
-                recent_commit_count = 10,
-            },
-            -- Customize sections
-            sections = {
-                untracked = {
-                    folded = false,
-                    hidden = false,
-                },
-                unstaged = {
-                    folded = false,
-                    hidden = false,
-                },
-                staged = {
-                    folded = false,
-                    hidden = false,
-                },
-            },
-            -- Disable line numbers in Neogit buffers
-            disable_line_numbers = true,
-            -- File watcher configuration
-            filewatcher = {
-                interval = 100,
-                enabled = true,
-            },
-            -- Mappings
-            mappings = {
-                -- Modify the push mapping to return to neo-tree
-                push = function(popup)
-                    neogit.push(popup)
-                    vim.defer_fn(function()
-                        vim.cmd 'Neotree reveal'
-                    end, 1000) -- Adjust the delay as needed
-                end,
-            },
-        }
-    end,
+    config = true,
+    kind = 'auto',
     keys = {
-        { '<leader>g', ':Neogit kind=vsplit<CR>', desc = 'Neogit' },
+        { '<leader>g', ':Neogit kin<CR>', desc = 'Neogit' },
         { '<leader>gc', ':Neogit commit<CR>', desc = 'Neogit Commit' },
         { '<leader>gp', ':Neogit push<CR>', desc = 'Neogit Push' },
     },
+    filewatcher = {
+        interval = 100,
+        enabled = true,
+    },
+    integrations = {
+        telescope = true,
+        diffview = true,
+        fzf_lua = true,
+    },
+    disable_line_numbers = true,
 }
