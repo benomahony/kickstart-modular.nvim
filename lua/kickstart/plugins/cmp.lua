05c1bcc8ca9aa1fac45f849013a6d4687adc61c7
Ben O'Mahony <ben@jobsasi.com>
2024-10-12 14:04:41 +0100

    Removing more comments

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -1,4 +1,3 @@
--- TODO: get friendly snippets
 return {
     { -- Autocompletion
         'hrsh7th/nvim-cmp',
@@ -20,9 +19,6 @@ return {
                     return 'make install_jsregexp'
                 end)(),
                 dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
                     {
                         'rafamadriz/friendly-snippets',
                         config = function()
@@ -33,9 +29,6 @@ return {
             },
             'saadparwaiz1/cmp_luasnip',
 
-            -- Adds other completion capabilities.
-            --  nvim-cmp does not ship with all sources by default. They are split
-            --  into multiple repos for maintenance purposes.
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
         },
